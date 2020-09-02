6f9acba0b0cc23ce4e9c6a15f723b49e8f91c0a7
Eduardo Nicoletti <JKGaming4@outlook.com>
2020-09-01 23:18:17 -0300

    Fix

---
+++ Barbaro.gd
@@ -0,0 +1,16 @@
+extends "res://Personage.gd"
+
+# Public Identifiers
+
+# Private Identifiers
+
+# Methods Created
+
+# Standard Methods
+func _ready():
+	id = 3
+	var player_vars = get_node("/root/PlayerVariables")
+	if(player_vars.playerOneChar == id):
+		playerOne()
+	else:
+		playerTwo()
